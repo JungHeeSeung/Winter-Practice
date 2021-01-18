78921940ed4769b9d87d94f80840936ae44a60fc
JungHeeSeung <moss2223@naver.com>
2021-01-18 11:21:53 +0900

    1/18
    잘 될 때 세이브

---
+++ Nreal/DinosaurExhibition/Assets/Script/ObjController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NRKernal;
+using NRKernal.NRExamples;
+
+public class ObjController : MonoBehaviour
+{
+    public TrackingImageExampleController trackingImage;
+
+    private Touch OldTouch;
+
+    public float rotSpd = 0.1f;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        var target = trackingImage.data;
+
+        if (target != null)
+        {
+            // 딕셔너리의 밸류를 가져왔음
+            // 아마도.. 여러 종류 이미지 트래킹이 필요하면 바뀔지도...??
+
+            foreach (var TargetVal in target.Values)
+            {
+                if (TargetVal.Image != null)
+                {
+                    if (Input.touchCount > 0)
+                    {
+                        Touch touch = Input.GetTouch(0);
+                        if (touch.phase == TouchPhase.Began)
+                        {
+                            OldTouch = touch;
+                        }
+                        else if(touch.phase == TouchPhase.Moved)
+                        {
+
+                            float deltaX = OldTouch.position.x - touch.position.x;
+                            float deltaY = OldTouch.position.y - touch.position.y;
+
+                            float rotX = deltaX * Time.deltaTime * rotSpd;
+                            float rotY = deltaY * Time.deltaTime * rotSpd;
+                         
+                            TargetVal.Obj.transform.Rotate(Vector3.up, rotX);
+                            TargetVal.Obj.transform.Rotate(Vector3.forward, rotY);
+
+                            OldTouch = touch;
+                        }
+                    }
+                }
+            }
+
+            if(NRInput.GetButton(ControllerButton.HOME))
+            {
+                foreach (var TargetVal in target.Values)
+                {
+                    TargetVal.Obj.transform.rotation = Quaternion.identity;
+                }
+            }
+        }
+    }
+
+   
+       
+   
+}
---
+++ Nreal/DinosaurExhibition/Assets/Script/TestScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    public float rotSpd = 1f;
+
+    Vector3 oldPos;
+
+    bool isMouseDown = false;
+
+    float rotX;
+    float rotY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 마우스가 눌림
+        if (Input.GetMouseButtonDown(0))
+        {
+            oldPos = Input.mousePosition;
+            isMouseDown = true;
+        }
+
+        // 마우스가 떼짐
+        if (Input.GetMouseButtonUp(0))
+        {
+            isMouseDown = false;
+        }
+
+        if (isMouseDown)
+        {
+            var newPos = Input.mousePosition;
+
+            float deltaX = newPos.x - oldPos.x;
+            float deltaY = newPos.y - oldPos.y;
+
+            rotX += deltaX * rotSpd * Time.deltaTime;
+            rotY += deltaY * rotSpd * Time.deltaTime;
+
+            gameObject.transform.Rotate(Vector3.up, rotX);
+            gameObject.transform.Rotate(Vector3.forward, rotY);
+           
+            //gameObject.transform.rotation = Quaternion.Euler(rotY, rotX, 0f);
+
+            oldPos = newPos;
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.up);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.right);
+    }
+}
